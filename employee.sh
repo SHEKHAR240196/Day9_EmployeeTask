63aef0ca3b7a320371209a70d5a97ff9190259ef
shekhar <shekharpatle999@gmail.com>
2021-04-03 12:57:12 +0530

    Added: storethedaily

--- employee.sh
+++ employee.sh
@@ -1,20 +1,40 @@
-#! /bin/bash
+#! /bin/bash -x
 
 isPartTime=1
 isFullTime=2
+maxHrsInMonth=4
 empRatePerHr=20
-randomCheck=$((RANDOM%3))
+numWorkingDays=20
 
-if [ $isFullTime -eq $randomCheck ]
-then
-	empHrs=8
-elif [ $isPartTime -eq $randomCheck ]
-then
-	empHrs=4
-else
-	empHrs=0
-fi
+totalEmpHrs=4
+totalWorkingDays=0
 
-salary=$(($empHrs*$empRatePerHr))
+function getWorkHrs() {
+	local $empCheck=$1
+	case $empCheck in
+		$isFullTime) empHrs=8 ;;
+		$isPartTime) empHrs=4 ;;
+		*) empHrs=0 ;;
+	esac
+	echo $empHrs
+}
 
-echo "Part Time Employee Wage :" $salary
+function getEmpWage() {
+	local empHr=$1
+	echo $(($empHr*$empRatePerHr))
+}
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWokingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+	empHrs="$( getWorkingHours $empCheck )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	dailyWage[$totalWorkingDays]="$( getEmpWage $empHrs )"
+
+done
+
+totalSalary=$(($totalEmpHrs*$empRatePerHr))
+
+echo ${dailyWage[@]}
+echo $totalSalary
